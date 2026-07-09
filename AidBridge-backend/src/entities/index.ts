d428950a12989bd56d98e1316d4666d47af6c6c3
ayo-ola0710 <israelolatunle2015@gmail.com>
2026-07-09 07:55:49 +0100

    feat: Setup database modules and migrations

---
+++ AidBridge-backend/src/entities/index.ts
@@ -0,0 +1,7 @@
+export { User } from './user.entity';
+export { NGO } from './ngo.entity';
+export { Donation } from './donation.entity';
+export { Volunteer } from './volunteer.entity';
+export { Task } from './task.entity';
+export { TaskSubmission } from './task-submission.entity';
+export { Reward } from './reward.entity';
